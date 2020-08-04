f317be9f36ff4f8b891bc4fa5fe8098c273f1a31
Denis Corbin <dar.linux@free.fr>
2020-08-04 11:26:21 +0200

    fixing typo in error message

--- src/condition.cpp
+++ src/condition.cpp
@@ -43,7 +43,7 @@ namespace libthreadar
     condition::condition(unsigned int num): cond(num), counter(num)
     {
 	if(num < 1)
-	    throw exception_range("need at least one instance for to create a condition object");
+	    throw exception_range("need at least one instance to create a condition object");
 
 	for(unsigned int i = 0; i < num; ++i)
 	{
