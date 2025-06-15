14f7ffb0343e6baead55643f0e06f4a50c012313
Denis Corbin <dar.linux@free.fr>
2025-06-15 18:57:41 +0200

    adding stack_size example program

---
+++ doc/examples/stack_sizer.cpp
@@ -0,0 +1,70 @@
+/*********************************************************************/
+// libthreadar - is a library providing several C++ classes to work with threads
+// Copyright (C) 2014-2024 Denis Corbin
+//
+// This file is part of libthreadar package
+//
+//  libthreadar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  libhtreadar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with libthreadar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+#include <iostream>
+#include "../../src/libthreadar.hpp"
+
+size_t get_default_stack_size();
+
+class my_thread: public libthreadar::thread_signal
+{
+protected:
+
+    virtual void inherited_run() override { std::cout << "default thread size reported get_stacksize() from within stack: " << get_default_stack_size() << std::endl; };
+};
+
+size_t get_default_stack_size()
+{
+    size_t ret = 0;
+    void* ptr = nullptr;
+    pthread_attr_t attr;
+
+    switch(pthread_attr_init(&attr))
+    {
+    case 0:
+	break;
+    default:
+	return 0;
+    }
+
+    pthread_attr_getstack(&attr,
+			  &ptr,
+			  &ret);
+
+    pthread_attr_destroy(&attr);
+
+    return ret;
+}
+
+int main()
+{
+    my_thread t1;
+
+    std::cout << "default stack size is: " << get_default_stack_size() << std::endl;
+
+    t1.run();
+    t1.join();
+    t1.set_stack_size(4*1024*1024);
+    t1.run();
+    t1.join();
+}
